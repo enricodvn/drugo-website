7b7f98fa55d35f8af53f1f9f15ca751f56a0c9d6
enricodvn <enricodvn@gmail.com>
2023-05-03 14:51:23 -0300

    feat: adds navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,47 @@ function App() {
 
   return (
     <>
-      <div>
+      <header class="navbar nav">
+        <nav aria-label="Site sections">
+          <ul role="list">
+            <li><a href="/me">About me</a></li>
+            <li><a href="#">Articles</a></li>
+          </ul>
+        </nav>
+        <nav class="pull-right" aria-label="Social media links">
+          <ul role="list">
+            <li>
+              <a href="https://twitter.com/EnricoDVN" target="_blank">
+                <img src={twitterLogo} className="logo medialogo" alt="Twitter logo" />
+              </a>
+            </li>
+            <li>
+              <a href="https://br.linkedin.com/in/enrico-davini-neto" target="_blank">
+                <img src={linkedinLogo} className="logo medialogo" alt="LinkedIn logo" />
+              </a>
+            </li>
+            <li>
+              <a href="https://unsplash.com/@enricodvn" target="_blank">
+                <img src={unsplashLogo} className="logo medialogo" alt="Unsplash logo" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/enricodvn" target="_blank">
+                <img src={githubLogo} className="logo medialogo" alt="Github logo" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div class="content">
       <a href="">
         <img src={drugoLogo} className="roundlogo" alt="Drugo logo" />
       </a>
-      <h2> Hi! My name is Enrico but people call me Drugo. </h2>
 
       <h2> Welcome to my personal website! </h2>
 
-      <p>Will be blog posting soon...
-      but in the meantime follow me on the social media:</p>
-      <p>
-      <a href="https://twitter.com/EnricoDVN" target="_blank">
-        <img src={twitterLogo} className="logo medialogo" alt="Twitter logo" />
-      </a>
-      <a href="https://br.linkedin.com/in/enrico-davini-neto" target="_blank">
-        <img src={linkedinLogo} className="logo medialogo" alt="LinkedIn logo" />
-      </a>
-      <a href="https://unsplash.com/@enricodvn" target="_blank">
-        <img src={unsplashLogo} className="logo medialogo" alt="Unsplash logo" />
-      </a>
-      <a href="https://github.com/enricodvn" target="_blank">
-        <img src={githubLogo} className="logo medialogo" alt="Github logo" />
-      </a>
-      </p>
+      <p>Hi! My name is Enrico but people call me Drugo.</p>
+
       </div>
     </>
   )
