1d126ed2abf9dc5535ed9d68e9fd5cbec94945fb
enricodvn <enricodvn@gmail.com>
2023-05-05 10:17:54 -0300

    fix: makes category page use Link when rendering a nodes, to fix navigation

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Category() {
 
   const { category } = useParams();
 
-  const [articleMd, setArticleMd] = useState('');
+  const [categoryMd, setCategoryMd] = useState('');
 
   useEffect(() => {
     fetch(`/articles/${category}/en.md`)
       .then(res => res.text())
-      .then(res => setArticleMd(res));
+      .then(res => setCategoryMd(res));
   });
 
   return (
     <div className="content">
-      <ReactMarkdown children={articleMd} />
+      <ReactMarkdown
+      components={{
+        // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
+        a: ({node, ...props}) => <Link to={props.href} {...props} />
+      }}
+      children={categoryMd} />
     </div>
   )
 }
